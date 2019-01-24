fd84c3c462e9534e02666cd06b83a4c433e5fb61
chris <cj_holmer2@yahoo.com>
2019-01-24 13:59:05 -0800

    Created platform rule tile and started 2nd scene
    - 2nd scene for protocol "crow"
    - created a shuttle door that opens and switches scenes
    - new platform sprites (will need more for tilemap)
    - platform rule tile
    - everything new is still hidden in animation folder

---
+++ Assets/Animations/Player1/ShuttleDoor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShuttleDoor : MonoBehaviour
+{
+    public Sprite openSprite;
+    public Sprite closeSprite;
+    public string gotoScene;
+    public bool open = false;
+
+    void OpenDoor()
+    {
+        GetComponent<SpriteRenderer>().sprite = openSprite;
+        GetComponent<Collider2D>().isTrigger = true;
+    }
+
+    void CloseDoor()
+    {
+        GetComponent<SpriteRenderer>().sprite = closeSprite;
+        GetComponent<Collider2D>().isTrigger = false;
+    }
+
+    void OnValidate()
+    {
+        if(open)
+        {
+            OpenDoor();
+        }
+        else
+        {
+            CloseDoor();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(gotoScene);
+        }
+    }
+}
